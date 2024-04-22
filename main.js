a7fab261b79e46002003424115031af2ccc4fae8
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-23 04:55:20 +0500

    TO DO APPLICATION

---
+++ main.js
@@ -0,0 +1,22 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let todoList = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todoList",
+            message: "What you want to add in your todoList?",
+            type: "input",
+        },
+        {
+            name: "addMore",
+            message: "Do you want to add more in your todoList?",
+            type: "confirm",
+            default: "true",
+        },
+    ]);
+    todoList.push(addTask.todoList);
+    condition = addTask.addMore;
+    console.log(todoList);
+}
